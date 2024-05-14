f39b17c7008ccc4dc38bf1e5e990b68eb73e87f0
Jisna George <jisnageorge0143@gmail.com>
2024-05-14 10:10:10 +0530

    initial commit

---
+++ products.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetchProducts();
+});
+
+function fetchProducts() {
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(products => displayProducts(products))
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+function displayProducts(products) {
+    const container = document.getElementById('products-container');
+    container.innerHTML = ''; 
+
+    products.forEach(product => {
+        const cardHTML = `
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="${product.image}" class="card-img-top" alt="${product.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.title}</h5>
+                        <p class="card-text">${product.price}</p>
+                        <a href="#" class="btn btn-primary">Buy Now</a>
+                    </div>
+                </div>
+            </div>
+        `;
+        container.innerHTML += cardHTML;
+    });
+}
